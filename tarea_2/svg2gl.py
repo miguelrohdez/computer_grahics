876e67776aa0d0f2ce67eefabd2f768d0c37f48d
Luis Alberto Oropeza Vilchis <luis.oropeza.129@gmail.com>
2017-10-14 21:56:06 -0500

    Refactorizando el codigo

--- tarea_2/svg2gl.py
+++ tarea_2/svg2gl.py
@@ -8,30 +8,26 @@ class Poligono():
         self.color = color
 
     def __str__(self):
-        rep = '* Poligono\n'
-        for punto in self.vertices:
-            rep += str(punto)
-            rep += '\n'
-        rep += 'Color {0}\n'.format(self.color)
-        rep += '* Fin Poligono\n'
-        return rep
+        cabecera = '* Poligono\n'
+        color = 'Color {0}\n'.format(self.color)
+        vertices = ["".join([str(v), '\n']) for v in self.vertices]
+        return "".join([cabecera, color] + vertices)
 
-    def to_gl(self):
-        rep = 'glBegin(GL_POLYGON);\n'
-        rep += '\tglColor3f({0}, {1}, {2});\n'.format(self.color[0],\
-                                                     self.color[1],\
-                                                     self.color[2])
-        for punto in self.vertices:
-            rep += '\t'
-            rep += punto.to_gl()
-        rep += 'glEnd();\n'
-        return rep
+    def to_opengl(self):
+        begin = 'glBegin(GL_POLYGON);\n'
+        end = 'glEnd();\n'
+        color = '\tglColor3f({0}, {1}, {2});\n'.format(self.color[0],\
+                                                       self.color[1],\
+                                                       self.color[2])
+        vertices = ["".join(['\t', p.to_opengl()]) for p in self.vertices]
+        return "".join([begin, color] + vertices + [end])
 
 class Vertice():
 
-    def __init__(self, x, y):
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x, y, z=-1.2, presicion=2):
+        self.x = round(x, presicion)
+        self.y = round(y, presicion)
+        self.z = z
 
     def __str__(self):
         return "({0}, {1})".format(self.x, self.y)
@@ -40,36 +36,35 @@ class Vertice():
         return (isinstance(other, self.__class__)) and \
         (self.x==other.x and self.y==other.y)
 
-    def to_gl(self):
-        return 'glVertex3f({0}, {1}, {2});\n'.format(self.x, self.y, -1.2)
+    def to_opengl(self):
+        return 'glVertex3f({0}, {1}, {2});\n'.format(self.x, self.y, self.z)
 
-
-def svg2gl(archivo):
+def svg2gl(nombre_archivo):
     poligonos = []
     puntos = []
-    objetos, atributos = svg2paths(archivo)
-    zipped = zip(objetos, atributos)
-    for objeto in zipped:
-        for linea in objeto[0]:
-            for punto in linea:
-                vertice = Vertice(punto.real, punto.imag)
-                if vertice not in puntos:
-                    puntos.append(vertice)
-        color = objeto[1]['style'].split(';')[0].split(':')[1]
-        if '#' in color:
-            poligonos.append(Poligono(puntos, hex2rgb(color)))
-        else:
-            poligonos.append(Poligono(puntos))
+    objetos, atributos = svg2paths(nombre_archivo)
+    paths = zip(objetos, atributos)
+    for p in paths:
+        for linea in p[0]:
+            puntos += [Vertice(v.real, v.imag)\
+                          for v in linea if v not in puntos]
+        color = p[1]['style'].split(';')[0].split(':')[1]
+        poligonos.append(Poligono(puntos,
+                                  hex2float(color) if '#' in color else (0)*3))
         puntos = []
     return poligonos
 
 def hex2rgb(hex):
     tmp = hex.strip('#')
     rgb = tuple(int(tmp[i:i+2], 16) for i in (0, 2 ,4))
-    return rgb2float(rgb)
+    return rgb
 
 def rgb2float(rgb):
     return tuple(i/256 for i in rgb)
+
+def hex2float(hex):
+    rgb = hex2rgb(hex)
+    return rgb2float(rgb)
 
 def main():
     if len(sys.argv) < 2:
@@ -79,9 +74,9 @@ def main():
         fsalida = open('glvertex.txt', 'w')
     else:
         fsalida = open(sys.argv[2], 'w')
-    pols = svg2gl(sys.argv[1])
-    for i in pols:
-        fsalida.write(i.to_gl())
+    poligonos = svg2gl(sys.argv[1])
+    for i in poligonos:
+        fsalida.write(i.to_opengl())
     fsalida.close()
 
 if __name__ == '__main__':
