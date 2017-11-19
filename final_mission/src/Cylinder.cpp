cdcc12e5b1f92bb643e13c627c62290e3dfced08
Luis Alberto Oropeza Vilchis <luis.oropeza.129@gmail.com>
2017-11-19 13:07:37 -0600

    Cilindro acualizado

--- final_mission/src/Cylinder.cpp
+++ final_mission/src/Cylinder.cpp
@@ -61,36 +61,36 @@ void Cylinder::draw() {
 	float v5[] = {0.0, 0.0, 0.0};
 	float angle = 2 * PI / resolution;
 	//float ctext_s = 1/resolution-1;
-	float ctext_s = 1.0/resolution;
+	float ctext_s = 1.0 / resolution;
 	float ctext_t = 0.0;
 
 	for(int i = 0; i < resolution; i++) {
-		v2[0] = radioTop*cos(angle*i);
+		v2[0] = radioTop * cos(angle * i);
 		v2[1] = 0;
-		v2[2] = radioTop*sin(angle*i);
+		v2[2] = radioTop * sin(angle * i);
 
-		v3[0] = radioTop*cos(angle*(i+1));
+		v3[0] = radioTop * cos(angle * (i + 1));
 		v3[1] = 0;
-		v3[2] = radioTop*sin(angle*(i+1));
+		v3[2] = radioTop * sin(angle * (i + 1));
 
-		v4[0] = radioBottom*cos(angle*i);
+		v4[0] = radioBottom * cos(angle * i);
 		v4[1] = height;
-		v4[2] = radioBottom*sin(angle*i);
+		v4[2] = radioBottom * sin(angle * i);
 
-		v5[0] = radioBottom*cos(angle*(i+1));
+		v5[0] = radioBottom * cos(angle * (i + 1));
 		v5[1] = height;
-		v5[2] = radioBottom*sin(angle*(i+1));
+		v5[2] = radioBottom * sin(angle * (i + 1));
 
 		glBegin(GL_POLYGON);
 			glNormal3f( 0.0f, -1.0f, 0.0f);
-			glVertex3f(0.0,0.0,0.0);
+			glVertex3f(0.0, 0.0, 0.0);
 			glVertex3fv(v2);
 			glVertex3fv(v3);
 		glEnd();
 
 		glBegin(GL_POLYGON);
 			glNormal3f( 0.0f, 1.0f, 0.0f);
-			glVertex3f(0.0,height,0.0);
+			glVertex3f(0.0, height, 0.0);
 			glVertex3fv(v4);
 			glVertex3fv(v5);
 		glEnd();
@@ -98,10 +98,10 @@ void Cylinder::draw() {
 		glBindTexture(GL_TEXTURE_2D, texture.GLindex);
 		glBegin(GL_POLYGON);
 			glNormal3f( v2[0], 0.0f, v2[2]);
-			glTexCoord2f(ctext_s*i, 0.0f);		glVertex3fv(v2);
-			glTexCoord2f(ctext_s*(i+1), 0.0f);	glVertex3fv(v3);
-			glTexCoord2f(ctext_s*(i+1), 1.0f);	glVertex3fv(v5);
-			glTexCoord2f(ctext_s*i, 1.0f);		glVertex3fv(v4);
+			glTexCoord2f(ctext_s * i, 0.0f);		glVertex3fv(v2);
+			glTexCoord2f(ctext_s * (i + 1), 0.0f);	glVertex3fv(v3);
+			glTexCoord2f(ctext_s * (i + 1), 1.0f);	glVertex3fv(v5);
+			glTexCoord2f(ctext_s * i, 1.0f);		glVertex3fv(v4);
 		glEnd();
 	}
 }
